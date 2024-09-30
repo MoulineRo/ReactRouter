50d432916e9c8171bd66317237a77543ea651b48
Dmytro Prokudin <pro.diyezz@yahoo.com>
2024-09-30 14:15:32 +0200

    [HW27] Added review code with the example how to create a routing with a sidebar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import SidebarMenu from "./pages/sideBar";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./component/About";
+import Contact from "./component/Contact";
+import Home from "./component/Home";
+
 
 function App() {
   return (
+    <Router>
       <div className="App">
         <div className="sidebar_outlet">
           <div>
-            <SidebarMenu/>
+            <SidebarMenu />
           </div>
           <div className="outlet">
-            <Outlet/>
+            <Routes>
+              <Route path="/" element={<h3>Welcome</h3>} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
           </div>
         </div>
       </div>
+    </Router>
 
   );
 }
